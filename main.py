763f40b6e834f0cbf6ae74ffe1ffb7122b4a82f7
zZJoennZz <zzjoennzz@gmail.com>
2026-04-28 01:59:14 +0800

    Allowed all for tests and UI

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
 # --- CORS CONFIGURATION ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
