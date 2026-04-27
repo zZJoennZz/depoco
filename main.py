e593b6478bef2ca1fcfe7b7b8e43f1c511e9a728
zZJoennZz <zzjoennzz@gmail.com>
2026-04-28 03:56:59 +0800

    Changed the list for allow_origins

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ origins = [
 # --- CORS CONFIGURATION ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
